a183f9cd7488760dd4f40e5e0be92ac1cc041bc7
Mohanned AlNouman <mohanned.alnouman@gmail.com>
2020-10-14 19:49:58 -0400

    forgot to run build

---
+++ client/build/precache-manifest.a45c31b9db67ce60bbc900ebf70a0bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d125154f6215130d95fe5dd884c8565",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a6751f83bdb4b0081aa",
+    "url": "/static/js/2.7b188d5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7b188d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e30c6d6441d63e37c98",
+    "url": "/static/js/main.0a82d843.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
